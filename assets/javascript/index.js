14da78dac39e5fddbc7c2ac39723cee023b0e88d
Mirian <mirianejerenwa@yahoo.com>
2018-11-04 18:54:07 -0500

    updates

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -14,15 +14,23 @@ for (i = 0; i < coll.length; i++) {
     });
   }
 
-// landing page
+  const enterCart = document.querySelectorAll('#addToCart');
+  enterCart.addEventListener('click', function() {
+    $('#addToCart').on('click', function () {
+        console.log('hello');
+        
+    })
+      enterCart.classList.toggle('active');
+     
+  });
 
-const displayItems1 = function () {
-    hideAll();
-    $('.dropdown').render();
-}
+// const displayItems1 = function () {
+//     hideAll();
+//     $('.dropdown').render();
+// }
     
-$('.dropdown').on('click',displayItems1);
-$('.details').on('click',view);
+// $('.dropdown').on('click',displayItems1);
+// $('.details').on('click',view);
 // user selects an item from the left, it will be added and updated to the shopping cart.
 // user attempts to add an item that already exists in their shopping cart, a message will be displayed alerting them that that item has already been added.
 // user will be able to click items to remove it from their shopping cart.
