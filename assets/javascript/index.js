e2a50bbdb55f18e36611005d0260a73366b58ac6
Mirian <mirianejerenwa@yahoo.com>
2018-11-04 01:09:04 -0400

    hover

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,4 @@
+// user will be presented with a list of items on the left as well as a shopping cart on the right.
 // How the list looks
 const coll = document.querySelectorAll('.collapsibleList');
 var i;
@@ -22,7 +23,6 @@ const displayItems1 = function () {
     
 $('.dropdown').on('click',displayItems1);
 $('.details').on('click',view);
-// user will be presented with a list of items on the left as well as a shopping cart on the right.
 // user selects an item from the left, it will be added and updated to the shopping cart.
 // user attempts to add an item that already exists in their shopping cart, a message will be displayed alerting them that that item has already been added.
 // user will be able to click items to remove it from their shopping cart.
